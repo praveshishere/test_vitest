a2e000df4330fa9b1210ce2a20c0c62bd2b446e0
sahil <sahil@vidyamantra.com>
2023-06-17 18:02:06 +0530

    map-sahil

---
+++ abdullah/map.ts
@@ -0,0 +1,16 @@
+
+/**
+ * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+ * @param arr Array to map
+ * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+ */
+    
+export function map<T, U>(arr: T[], callbackfn: (value: T, index: number, array: T[]) => U): U[] {
+let tempArr = [];
+    for(let i =0;i<arr.length;i += 1){
+        tempArr.push(callbackfn(arr[i]));
+    }
+
+
+    return tempArr;
+}
