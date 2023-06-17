11ea1ce99c979365599f36f198115fe658d1a2ca
sahil <sahil@vidyamantra.com>
2023-06-17 17:14:06 +0530

    validate-password-sahil

--- helpers/validate_passpwrd.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest";
-import { validatePassword } from "./validate_password";
-describe("validatePassword", () => {
-    it("", () => {
-        expect(validatePassword("ddkj")).toBe(false);
-    })
-    // Write more test as required
-})
