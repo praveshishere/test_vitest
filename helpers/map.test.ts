d389b0575154b4bbdfed908bda4aa9cb14635528
vinoddotcom <vinod.yadav@vidyamantra.com>
2023-06-17 17:22:46 +0530

    Rfactred

--- helpers/map.test.ts
+++ helpers/map.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, expectTypeOf } from "vitest";
+import { describe, it, expect } from "vitest";
 import { map } from "./map";
 
 describe("map", () => {
@@ -8,70 +8,4 @@ describe("map", () => {
         })).toEqual([3, 4, 5]);
     })
     // Write more test as required
-
-    it("Empty array", () => {
-        expect(map([], (num) => {
-            return num + 2;
-        })).toEqual([]);
-    })
-
-    it("array with undefined", () => {
-        expect(map([undefined], (num) => {
-            return num + 2;
-        })).toEqual([NaN]);
-    })
-
-    it("Object Array", () => {
-        expect(map([{jj: "j"}, {jj: "hj"}], (item) => {
-            return item.jj;
-        })).toEqual(["jj", "hj"]);
-    })
-
-    it("Null array", () => {
-        expect(map([null], (num) => {
-            return num + 2;
-        })).toEqual([2]);
-    })
-
-    it("Nested null array", () => {
-        expect(map([[null]], (num) => {
-            // @ts-nocheck
-            return num + 2;
-        })).toEqual(["2"]);
-    })
-
-    it("Nested null array", () => {
-        expect(map([[null]], (num) => {
-            // @ts-nocheck
-            return num + 2;
-        })).toEqual(["2"]).toArray();
-    })
-
-    it("Multiple nested array", () => {
-        expect(map([[[[null]]]], (num) => {
-            // @ts-nocheck
-            return num + 2;
-        })).toEqual(["2"]);
-    })
-
-    it("Array with undefined and null", () => {
-        expect(map([undefined, null], (num) => {
-            // @ts-nocheck
-            return num + 2;
-        })).toEqual([NaN, 2]);
-    })
-
-    it("Nested Array with undefined and null", () => {
-        expect(map([[undefined], [null]], (num) => {
-            // @ts-nocheck
-            return num + 2;
-        })).toEqual(["2", "2"] );
-    })
-
-    it("Nested Array with undefined and null", () => {
-        expectTypeOf(map([[undefined], [null]], (num) => {
-            // @ts-nocheck
-            return num;
-        })).toBeArray();
-    })
 })
---
+++ vinod/map.test.ts
@@ -0,0 +1,78 @@
+import { describe, it, expect, expectTypeOf } from "vitest";
+import { map } from "../helpers/map";
+
+describe("map", () => {
+    it("number add test", () => {
+        expect(map([1, 2, 3], (num) => {
+            return num + 2;
+        })).toEqual([3, 4, 5]);
+    })
+    // Write more test as required
+
+    it("Empty array", () => {
+        expect(map([], (num) => {
+            return num + 2;
+        })).toEqual([]);
+    })
+
+    it("array with undefined", () => {
+        expect(map([undefined], (num) => {
+            // @ts-nocheck
+            return num + 2;
+        })).toEqual([NaN]);
+    })
+
+    it("Object Array", () => {
+        expect(map([{jj: "j"}, {jj: "hj"}], (item) => {
+            return item.jj;
+        })).toEqual(["jj", "hj"]);
+    })
+
+    it("Null array", () => {
+        expect(map([null], (num) => {
+            return num + 2;
+        })).toEqual([2]);
+    })
+
+    it("Nested null array", () => {
+        expect(map([[null]], (num) => {
+            // @ts-nocheck
+            return num + 2;
+        })).toEqual(["2"]);
+    })
+
+    it("Nested null array", () => {
+        expect(map([[null]], (num) => {
+            // @ts-nocheck
+            return num + 2;
+        })).toEqual(["2"]).toArray();
+    })
+
+    it("Multiple nested array", () => {
+        expect(map([[[[null]]]], (num) => {
+            // @ts-nocheck
+            return num + 2;
+        })).toEqual(["2"]);
+    })
+
+    it("Array with undefined and null", () => {
+        expect(map([undefined, null], (num) => {
+            // @ts-nocheck
+            return num + 2;
+        })).toEqual([NaN, 2]);
+    })
+
+    it("Nested Array with undefined and null", () => {
+        expect(map([[undefined], [null]], (num) => {
+            // @ts-nocheck
+            return num + 2;
+        })).toEqual(["2", "2"] );
+    })
+
+    it("Nested Array with undefined and null", () => {
+        expectTypeOf(map([[undefined], [null]], (num) => {
+            // @ts-nocheck
+            return num;
+        })).toBeArray();
+    })
+})
