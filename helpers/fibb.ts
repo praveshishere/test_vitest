a2e91d5bf3ef87b5ffca4837996011f37d8577f1
Prince Kunwar <princekunwar6@gmail.com>
2023-06-17 16:27:41 +0530

    added map test

--- helpers/fibb.ts
+++ helpers/fibb.ts
@@ -3,6 +3,7 @@
  * Returns the nth fibbonaci number where 
  * 1st and 2nd number in sequence are 0 and 1
  * respectively
+ * refer: https://www.cuemath.com/numbers/fibonacci-sequence/
  * 
  * @param n the position of fibbonaci number 
  * @returns the nth fibbonaci number
---
+++ helpers/map.ts
@@ -0,0 +1,10 @@
+
+/**
+ * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+ * @param arr Array to map
+ * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+ */
+    
+export function map<T, U>(arr: T[], callbackfn: (value: T, index: number, array: T[]) => U): U[] {
+    return [];
+}
