9f816bd595ad06f44ad894eb6ca9dc39dd9c25ff
vipul Jaiswal <jaiswalvipul8@gmail.com>
2023-06-17 17:14:11 +0530

    test-cases-implemented

--- helpers/validate_passpwrd.test.ts
+++ helpers/validate_passpwrd.test.ts
@@ -1,8 +1,21 @@
 import { describe, it, expect } from "vitest";
 import { validatePassword } from "./validate_password";
 describe("validatePassword", () => {
-    it("", () => {
-        expect(validatePassword("ddkj")).toBe(false);
+    it("should has 8 chracters whether any conditions matches or not", () => {
+        expect(validatePassword("ddkj*12")).toBe(false); // asterisk and number condition matches
+        expect(validatePassword("ddkjbnmj*12")).toBe(true);
+    })
+
+    it("should has one asterisk whether any conditions matches or not", () => {
+        expect(validatePassword("ddkjbubh12")).toBe(false); // number and length property matches
+        expect(validatePassword("ddkjbubh**12")).toBe(false); // more than one asterisk
+        expect(validatePassword("ddkjbubh*12")).toBe(true);
+    })
+
+    it("should has atlest 2 numbers whether any conditions matches or not", () => {
+        expect(validatePassword("ddkjbubh*")).toBe(false); // no numbers present
+        expect(validatePassword("ddkjbubh*1")).toBe(true); // one number present
+        expect(validatePassword("ddkjbubh*123")).toBe(true);
     })
     // Write more test as required
 })
